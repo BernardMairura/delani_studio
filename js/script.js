01ac58f5d9808d3f6cbba5d898f6c0344a08313d
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-08-28 20:12:37 +0300

    form to my page/Prevent default for the submit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+
+const name=document.getElementById("username")
+const email=document.getElementById("email")
+const message=document.getElementById("msg")
+const errorElement=document.getElementById("error")
+
+FormData.addEventListener("submit",(e)=>{
+   
+
+let messages=[]
+if (name.value=== "" || name.value===null)
+
+e.preventDefault(){
+    messages.push("name is required")
+}
+})
