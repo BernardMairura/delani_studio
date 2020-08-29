5fef9e8d8f5120f88cbb6e7690ebc489dfb571e7
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-08-29 16:02:31 +0300

    add How to make elements appear and disappear in jQuery

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 
-const name=document.getElementById("username")
+/* const name=document.getElementById("username")
 const email=document.getElementById("email")
 const message=document.getElementById("msg")
 const errorElement=document.getElementById("error")
@@ -14,10 +14,18 @@ if (name.value=== "" || name.value===null)
 e.preventDefault(){
     messages.push("name is required")
 }
-})
+}) */
 
 $(document).ready(function(){
 
-$("#")
+$("#design").click(function(){
+    $("#designW").toggle();
+});
+$("#dev").click(function(){
+    $("#devW").toggle();
+});
+$("#product").click(function(){
+    $("#productW").toggle();
+});
 
 });
